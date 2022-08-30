13cdf5f6e8b10eca7e4641d7bbc6b9180b97d38e
Matthew <67935344+grievem@users.noreply.github.com>
2022-08-29 22:44:50 -0400

    JS Class 1

---
+++ script1.js
@@ -0,0 +1,43 @@
+let clientName = 'John'
+let clientEmail = 'John@email.com'
+let clientPackage = 'Premium'
+let clientAddress = '123 ABC Street'
+let clientCity = 'San Francisco'
+let clientState = 'California'
+let clientCountry = 'USA'
+let clientPhone = '123.456.7890'
+let clientCreditCard = '1234567890123456'
+let clientCardexpiration = '10 24'
+let clientCardsecurity = '123'
+let clientUsername = 'John SDGKU'
+let clientPassword = 'SDGKUisthebest'
+let clientInterests1 = 'Action'
+let clientInterests2 = 'Romance'
+let clientInterests3 = 'Compedy'
+let clientInterests4 = 'Drama'
+let clientInterests5 = 'Documentary'
+let clientStartdate = '2022'
+let clientEnddate = '2023'
+
+document.write(`
+ <p>Name: ${clientName}</p>
+ <p>Email: ${clientEmail}</p>
+ <p>Package: ${clientPackage}</p>
+ <p>Address: ${clientAddress}</p>
+ <p>City: ${clientCity}</p>
+ <p>State: ${clientState}</p>
+ <p>Country: ${clientCountry}</p>
+ <p>Phone: ${clientPhone}</p>
+ <p>Credit Card: ${clientCreditCard}</p>
+ <p>Cardexpiration: ${clientCardexpiration}</p>
+ <p>Cardsecurity: ${clientCardsecurity}</p>
+ <p>Username: ${clientUsername}</p>
+ <p>Password: ${clientPassword}</p>
+ <p>Interests: ${clientInterests1}</p>
+ <p>Interests: ${clientInterests2}</p>
+ <p>Interests: ${clientInterests3}</p>
+ <p>Interests: ${clientInterests4}</p>
+ <p>Interests: ${clientInterests5}</p>
+ <p>Startdate: ${clientStartdate}</p>
+ <p>Enddate: ${clientEnddate}</p>
+ `);
